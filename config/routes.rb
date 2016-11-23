266cfe9e43282096af041a87d968a6775b8c0855
Imre Mocsari <imremocsari1@yahoo.co.uk>
2016-11-23 14:49:17 +0000

    couriers route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: [:show, :new, :create, :destroy]
   get '/dashboard', to: 'users#dashboard'
-
+  resources :couriers
   resources :bookings do
     resources :confirmation, only: [:index] do
       resources :payment, only: [:index]
