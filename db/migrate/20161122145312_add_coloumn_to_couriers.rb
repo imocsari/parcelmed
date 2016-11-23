d6139d66bf778f2fefe628f5bd184d0af8ce45b2
Imre Mocsari <imremocsari1@yahoo.co.uk>
2016-11-23 11:22:15 +0000

    Courier seed files added

--- db/migrate/20161122145312_add_coloumn_to_couriers.rb
+++ db/migrate/20161122145312_add_coloumn_to_couriers.rb
@@ -1,10 +1,6 @@
 class AddColoumnToCouriers < ActiveRecord::Migration[5.0]
   def change
     add_column :couriers, :price, :integer
-<<<<<<< HEAD
-    add_column :couriers, :availability, :boolean, default: false
-=======
     add_column :couriers, :availability, :boolean
->>>>>>> master
   end
 end
