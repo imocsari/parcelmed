9d8820dbbedf6779f0f63168944bd6cc1ef6cb17
antonio59 <smith_antonio59@hotmail.com>
2016-11-24 12:05:57 +0000

    sidekiq

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings
   has_many :couriers, through: :bookings
+
+  after_save :async_update # Run on create & update
+
+  private
+
+  def async_update
+    UpdateUserJob.perform_later(self.id)
+  end
 end
