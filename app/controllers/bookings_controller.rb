c98bd1ce78e403d5fd19a63b4faecec73e0b5f1f
antonio59 <smith_antonio59@hotmail.com>
2016-11-28 15:33:42 +0000

    updated db with new fields to break down pick_up and destination

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,7 +7,6 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
-    @user = User.new
   end
 
   def show
@@ -16,15 +15,45 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+
     if @booking.save!
       UserMailer.welcome(booking_params[:email]).deliver_now
       redirect_to booking_path(@booking)
+    else
+      render :new
     end
   end
 
 private
 
   def booking_params
-    params.require(:booking).permit(:extra_packaging, :pick_up_contact_address, :destination_contact_address, :pick_up_contact_name, :destination_contact_name, :pick_up_contact_email, :destination_contact_email, :pick_up_contact_phone, :destination_contact_phone, :special_requirements, :hazardous_material)
+    params.require(:booking).permit(
+      :status,
+      :quantity,
+      :extra_packaging,
+      :pick_up_contact_address,
+      :destination_contact_address,
+      :pick_up_contact_name,
+      :destination_contact_name,
+      :pick_up_contact_email,
+      :destination_contact_email,
+      :pick_up_contact_phone,
+      :destination_contact_phone,
+      :special_requirements,
+      :hazardous_material,
+      :terms_and_conditions,
+      :pick_up_postcode,
+      :destination_postcode,
+      :pick_up_company_name,
+      :destination_company_name,
+      :pick_up_town,
+      :pick_up_county,
+      :pick_up_country,
+      :destination_town,
+      :destination_county,
+      :destination_country,
+      :pick_up_department,
+      :destination_department
+    )
   end
 end
